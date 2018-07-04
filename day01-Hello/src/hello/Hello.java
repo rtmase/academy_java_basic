cc96104636a01b1cfcb71db75558cfcac9ade8d8
rtmase <rtasem@gamil.com>
2018-07-04 11:54:20 +0900

    java_day01 기본형 boolean, char

---
+++ day01-Hello/src/hello/Hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// 자바 기본 출력 구문
+		// sysout => sysout => Ctrl + space bar 키 입력 후 code assistant
+		System.out.println("Hello, World!");
+		// 실행 Ctrl + F11
+		
+		
+	}
+
+}
---
+++ day01-Hello/src/type/primitive/BooleanTest.java
@@ -0,0 +1,25 @@
+package type.primitive;
+/**
+ * 자바의 유일한 논리형 값 : boolean
+ * 저장될 수 있는 값 : true / false
+ * boolean 타입을 테스트 해보는 클래스
+ * @author PC38221
+ *
+ */
+public class BooleanTest {
+	public static void main(String[] args) {
+		// 1. 선언 : 타입 변수이름;
+		boolean flag;
+		// boolean flag; 선언은 한번만 가능
+		
+		// 2. 초기화 : true 값 할당
+		flag = true;
+		
+		// 3. 사용 : flag 변수의 값을 출력
+		System.out.println("boolean flag = " + flag);
+		
+		
+		
+		
+	}
+}
